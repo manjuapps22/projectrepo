4c0f5402dfa5e63394589856ab9803d09cc28445
manjuapps22 <81699226+manjuapps22@users.noreply.github.com>
2022-12-10 20:33:10 +0530

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,4 +1,3 @@
-<code style="font-size: 10.2375px; font-family: Consolas, Menlo, Monaco, "Courier New", monospace; color: rgba(51, 51, 51, 0.85); background-color: rgb(246, 247, 248); border-radius: 2px; display: block; line-height: 1.5; position: relative; top: 0px; outline: 0px !important;"> 
 package main
 import (
    "log"
@@ -15,4 +14,3 @@ func main() {
    http.Handle("/", s)
    log.Fatal(http.ListenAndServe(":8080", nil))
 }
-</code>
